e795382bd8b8cacafffeb6efe8be2aef2011b8bc
“zebiniso” <szebiniso0909@gmail.com>
2024-04-15 01:25:05 +0600

    fixed prettier

--- src/components/UI/button.tsx
+++ src/components/UI/button.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Button = () => {
-    return (
-        <button>
-            Click
-        </button>
-    );
+  return <button>Click</button>;
 };
 
 export default Button;
