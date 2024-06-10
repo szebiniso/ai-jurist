158b7d2ea471f10e418ea1101a523e18b3754e6c
“zebiniso” <szebiniso0909@gmail.com>
2024-06-10 22:44:08 +0600

    fixed

--- src/config/useAuthRedirect.tsx
+++ src/config/useAuthRedirect.tsx
@@ -6,18 +6,18 @@ import { useEffect, useState } from "react";
 
 export const useAuthRedirect = () => {
   const pathname = usePathname();
-  const [storedAccount, setStoredAccount] = useState<TAccount>();
+  const [storedAccount, setStoredAccount] = useState<TAccount | null>();
   // const storedAccount = localStorage.getItem("account");
   useEffect(() => {
     if (typeof window === undefined) return;
     setStoredAccount(JSON.parse(localStorage.getItem("account")!));
   }, []);
-  const profile = storedAccount ? storedAccount : null;
+  const profile = storedAccount;
 
   const pagesWithoutAuth = ["/login", "/register", "/"];
 
   if (profile == null) {
-    if (pagesWithoutAuth.includes(pathname)) return;
+    // if (pagesWithoutAuth.includes(pathname)) return;
     //
     // return "/login";
   } else if (pathname === "/login") {
